9a0ff79a777d69da199047270b7350ec56615b34
ThomasAnselmetto <t.anselmetto@gmail.com>
2023-04-03 08:54:36 +0200

    base

---
+++ database/migrations/2023_03_31_125913_create_songs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('songs', function (Blueprint $table) {
+            $table->id();
+            $table->string('title',50);
+            $table->string('album',50);
+            $table->string('author',50);
+            $table->string('editor',50);
+            $table->float('length',4,2);
+            $table->string('poster');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('songs');
+    }
+};
+            
---
+++ resources/views/traccia.md
@@ -0,0 +1,25 @@
+<!-- Ciao ragazzi!
+
+Esercizio di oggi: Laravel Backoffice CRUD
+nome repo: laravel-base-crud
+Ciao ragazzi,
+oggi create un nuovo progetto Laravel 9 per gestire un archivio di brani musicali. Ogni brano dovrà avere (almeno):
+
+title
+album
+author
+editor
+length
+poster
+
+Milestone 1
+Tramite gli appositi comandi artisan create un model con relativa migration e un resource controller.
+
+Milestone 2
+Iniziate a definire le prime operazioni CRUD con le relative view:
+index()
+show()
+
+Bonus:
+creare il seeder per la tabella songs utilizzando Faker.
+Buon lavoro e buon weekend! :party_wizard: (modificato) -->
